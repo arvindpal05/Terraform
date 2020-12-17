8bf58d5ca190f94a059ebeb9e2a83da526cb09a1
Arvind Singh Pal <arvindpal05@gmail.com>
2020-12-17 10:59:42 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,13 +1,33 @@
-
-resource "aws_instance" "demo01" {
+module "ec2" {
+  source  = "arvindpal05/ec2/aws"
+  version = "0.0.1"
   ami           = "ami-04d29b6f966df1537"
+  keyname       = "arvi_key"
   instance_type = "t2.micro"
+  noOfInstance =2
+  # insert the 4 required variables here
+}
+/*module "ec2" {
+  source        = "./module/ec2"
+  ami           = "ami-04d29b6f966df1537"
+  keyname       = "arvi_key"
+  instance_type = "t2.micro"
+  noOfInstance =2
 
-  tags = {
-    Name = "Arvind-EC2"
-  }
+}*/
+data "aws_availability_zones" "zone" {
   
-  vpc_security_group_ids = [aws_security_group.sg.id]
-  key_name = "arvi_key"  
+}
+data "aws_instance" "other"{
+        instance_id = "i-0b803f8c8e8dac9e1"
+}
+output "az" {
+        value = data.aws_availability_zones.zone.names[0]
 }
 
+output "ipaddresses" {
+        value = module.ec2.publicIPAddresses
+}
+output "sunainaipaddresses" {
+        value = data.aws_instance.other.public_ip
+}
--- provider.tf
+++ provider.tf
@@ -1,10 +1,4 @@
 provider "aws" {
-  region = "us-east-1"  
-}
-terraform {
-  backend "s3" {
-    bucket ="arvi-tfe-state-bucket"
-    key ="dev"
-    region ="us-east-1"
-  }
+  region  = "us-east-1"
+  profile = "training"
 }
