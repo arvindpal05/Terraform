2d0b8638309f04e72e81ae26f2b418a050247b05
Arvind Singh Pal <arvindpal05@gmail.com>
2020-12-17 11:01:11 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,33 +1,13 @@
-module "ec2" {
-  source  = "arvindpal05/ec2/aws"
-  version = "0.0.1"
-  ami           = "ami-04d29b6f966df1537"
-  keyname       = "arvi_key"
-  instance_type = "t2.micro"
-  noOfInstance =2
-  # insert the 4 required variables here
-}
-/*module "ec2" {
-  source        = "./module/ec2"
-  ami           = "ami-04d29b6f966df1537"
-  keyname       = "arvi_key"
-  instance_type = "t2.micro"
-  noOfInstance =2
 
-}*/
-data "aws_availability_zones" "zone" {
+resource "aws_instance" "demo01" {
+  ami           = "ami-04d29b6f966df1537"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Arvind-EC2"
+  }
   
-}
-data "aws_instance" "other"{
-        instance_id = "i-0b803f8c8e8dac9e1"
-}
-output "az" {
-        value = data.aws_availability_zones.zone.names[0]
+  vpc_security_group_ids = [aws_security_group.sg.id]
+  key_name = "arvi_key"  
 }
 
-output "ipaddresses" {
-        value = module.ec2.publicIPAddresses
-}
-output "sunainaipaddresses" {
-        value = data.aws_instance.other.public_ip
-}
--- provider.tf
+++ provider.tf
@@ -1,4 +1,10 @@
 provider "aws" {
-  region  = "us-east-1"
-  profile = "training"
+  region = "us-east-1"  
+}
+terraform {
+  backend "s3" {
+    bucket ="arvi-tfe-state-bucket"
+    key ="dev"
+    region ="us-east-1"
+  }
 }
