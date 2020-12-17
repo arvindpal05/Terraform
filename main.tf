d2f84ef117c0122371ccf3f03a3be5984d15704d
Arvind Singh Pal <arvindpal05@gmail.com>
2020-12-17 10:42:08 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,13 @@
+
+resource "aws_instance" "demo01" {
+  ami           = "ami-04d29b6f966df1537"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Arvind-EC2"
+  }
+  
+  vpc_security_group_ids = [aws_security_group.sg.id]
+  key_name = "arvi_key"  
+}
+
